84dddf24969630adcc92d85e63080e27d766ba54
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-11-07 19:42:54 -0600

    feat: add treesitter-context

--- lua/ani-nvim/packer.lua
+++ lua/ani-nvim/packer.lua
@@ -59,4 +59,5 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+    use { 'nvim-treesitter/nvim-treesitter-context' }
 end)
