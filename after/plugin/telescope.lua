99048073318cc67106fbe6debf96c10bd436bbcf
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-11-08 17:58:36 -0600

    feat: add useful keybindings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,17 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end, { desc = 'Telescope search in project files' })
+
+-- Insanely useful 
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Find [P]roject [F]iles' })
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Find [S]tring in [P]roject' })
+vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recently Opened Files ("." for repeat)' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+
+-- Nice to have
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+
+-- Open Telescope for Nvim config dir
+vim.keymap.set('n', '<leader>sn', function()
+    builtin.find_files { cwd = vim.fn.stdpath('config') }
+end, { desc = '[S]earch [N]eovim Config Files' })
