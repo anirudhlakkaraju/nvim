948a671371b9e0701a983e67f6e8fb16b492392e
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-11-27 18:20:28 -0600

    feat: remove unnecessary comments

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -12,12 +12,10 @@ require('gitsigns').setup {
         end
 
         -- Actions
-        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Git [S]tage Buffer' })
         map('n', '<leader>hs', gitsigns.stage_hunk, { desc = '[S]tage [H]unk under cursor' })
         map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = '[U]ndo [S]taged [H]unk under cursor' })
         map('n', '<leader>td', gitsigns.toggle_deleted, { desc = '[T]oggle [D]eleted Line Changes' })
         map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, { desc = '[S]tage [H]unk under cursor in Visual mode' })
-        map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, { desc = '[U]ndo [S]taged [H]unk under cursor in Visual mode' })
         map('n', '<leader>gb', function() gitsigns.blame_line { full = true } end, { desc = 'Preview [G]it [B]lame' })
         map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Show [H]unk Git [D]iff' })
     end
