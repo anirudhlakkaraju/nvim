d476746e8744dcfb5229d417aefc04a23402b0bd
Anirudh Lakkaraju <anirudhlakkaraju@Anirudhs-MBP.attlocal.net>
2025-01-17 12:59:57 -0600

    feat: reset hunk under cursor

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -13,6 +13,7 @@ require('gitsigns').setup {
 
         -- Actions
         map('n', '<leader>hs', gitsigns.stage_hunk, { desc = '[S]tage [H]unk under cursor' })
+        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = '[R]eset [H]unk under cursor' })
         map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = '[U]ndo [S]taged [H]unk under cursor' })
         map('n', '<leader>td', gitsigns.toggle_deleted, { desc = '[T]oggle [D]eleted Line Changes' })
         map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, { desc = '[S]tage [H]unk under cursor in Visual mode' })
