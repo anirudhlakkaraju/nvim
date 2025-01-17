9c30b95d8a1f7ac89b1f3be2f79eb300cd9db0b1
Anirudh Lakkaraju <anirudhlakkaraju@Anirudhs-MBP.attlocal.net>
2025-01-17 16:51:04 -0600

    feat: add dap and dap-go for debugging

--- lua/ani-nvim/packer.lua
+++ lua/ani-nvim/packer.lua
@@ -70,4 +70,6 @@ return require('packer').startup(function(use)
     }
     use { 'github/copilot.vim' }
     use { 'lewis6991/gitsigns.nvim' }
+    use { 'mfussenegger/nvim-dap' }
+    use { 'leoluz/nvim-dap-go' }
 end)
