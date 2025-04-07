98587c44d7984e936c63c467880f74a999d8b24f
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-04-07 15:00:18 -0500

    feat: install dap ui

--- lua/ani-nvim/packer.lua
+++ lua/ani-nvim/packer.lua
@@ -79,6 +79,10 @@ return require("packer").startup(function(use)
 	use({ "mfussenegger/nvim-dap" })
 	use({ "leoluz/nvim-dap-go" })
 	use({
+		"rcarriga/nvim-dap-ui",
+		requires = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+	})
+	use({
 		"stevearc/oil.nvim",
 		requires = { "nvim-tree/nvim-web-devicons", opt = true },
 	})
