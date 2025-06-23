7cff58d0db233783e0a4a68134f00a199ce286ad
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-06-22 19:18:05 -0500

    feat: set tabstop dynamically based on filetype

--- lua/ani-nvim/set.lua
+++ lua/ani-nvim/set.lua
@@ -4,8 +4,19 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
-vim.softtabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
+-- Custom tab width for js/ts files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+	callback = function()
+		vim.opt.tabstop = 2
+		vim.opt.shiftwidth = 2
+		vim.opt.softtabstop = 2
+	end,
+})
+
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
