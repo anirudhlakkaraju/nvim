94a919f050522a096b8da97b8da6d697ae1cb87a
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-07-15 11:35:34 -0500

    fix: remove mason lsp configsetup

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,9 +1,1 @@
 require("mason").setup({})
-require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "gopls", "pyright", "clangd" },
-	handlers = {
-		function(server_name)
-			require("lspconfig")[server_name].setup({})
-		end,
-	},
-})
