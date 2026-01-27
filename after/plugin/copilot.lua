5c6979ba4305dd35989a6b64d805f8e6346901ca
anirudhlakkaraju <lakanirudh123@gmail.com>
2026-01-27 11:48:03 -0600

    migrate from packer to lazy

--- after/plugin/copilot.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set("n", "<leader>ce", function()
-	vim.cmd("Copilot enable")
-end, { desc = "[C]opilot [E]nable" })
-
-vim.keymap.set("n", "<leader>cd", function()
-	vim.cmd("Copilot disable")
-end, { desc = "[C]opilot [D]isable" })
--- after/plugin/oil.lua
+++
@@ -1,9 +0,0 @@
-require("oil").setup({
-	default_file_explorer = false,
-	view_options = {
-		-- Show files and directories that start with "."
-		show_hidden = true,
-	},
-})
-
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open Oil File Explorer" })
