577e89195b7184daf3094e4396be38d4b8d690c9
anirudhlakkaraju <lakanirudh123@gmail.com>
2026-01-28 14:33:07 -0600

    fix: auto install missing parsers and highlight

--- lua/ani-nvim/lazy/treesitter.lua
+++ lua/ani-nvim/lazy/treesitter.lua
@@ -1,32 +1,42 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter").setup({
-			ensure_installed = {
-				"javascript",
-				"html",
-				"css",
-				"cpp",
-				"go",
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"markdown",
-				"markdown_inline",
-			},
-			sync_install = false,
-			auto_install = true,
-		})
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter").setup({
+            ensure_installed = {
+                "javascript",
+                "html",
+                "css",
+                "cpp",
+                "go",
+                "c",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "markdown",
+                "markdown_inline",
+            },
+            sync_install = false,
+            auto_install = true,
+        })
 
-		-- Enable treesitter highlighting for all filetypes
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = "*",
-			callback = function()
-				pcall(vim.treesitter.start)
-			end,
-		})
-	end,
+        -- Auto install missing parsers and enable treesitter highlighting
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "*",
+            callback = function()
+                local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+                if not lang then
+                    return
+                end
+
+                -- Install parser if not available
+                if not pcall(vim.treesitter.language.add, lang) then
+                    pcall(vim.cmd, "TSInstall " .. lang)
+                end
+
+                pcall(vim.treesitter.start)
+            end,
+        })
+    end,
 }
